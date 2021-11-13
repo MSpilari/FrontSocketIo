29d34c07b191881838e4bb4505463d87728b8d8b
MSpilari <mb.spilari@hotmail.com>
2021-11-13 12:44:43 -0300

    Finalizado a estrutura e a estilização do MessageList

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,5 +1,51 @@
+import logoImg from '../../assets/logo.svg'
+import styles from './styles.module.scss'
+
 const MessageList = () => {
-	return <h1>Message List Component</h1>
+	return (
+		<div className={styles.messageListWrapper}>
+			<img src={logoImg} alt='DoWhile Logo' />
+
+			<ul className={styles.messageList}>
+				<li className={styles.message}>
+					<p className={styles.messageContent}>
+						Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias
+						vel expedita vero culpa eum.
+					</p>
+					<div className={styles.messageUser}>
+						<div className={styles.userImage}>
+							<img src='https://github.com/MSpilari.png' alt='User Image' />
+						</div>
+						<span>MSpilari</span>
+					</div>
+				</li>
+				<li className={styles.message}>
+					<p className={styles.messageContent}>
+						Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias
+						vel expedita vero culpa eum.
+					</p>
+					<div className={styles.messageUser}>
+						<div className={styles.userImage}>
+							<img src='https://github.com/MSpilari.png' alt='User Image' />
+						</div>
+						<span>MSpilari</span>
+					</div>
+				</li>
+				<li className={styles.message}>
+					<p className={styles.messageContent}>
+						Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias
+						vel expedita vero culpa eum.
+					</p>
+					<div className={styles.messageUser}>
+						<div className={styles.userImage}>
+							<img src='https://github.com/MSpilari.png' alt='User Image' />
+						</div>
+						<span>MSpilari</span>
+					</div>
+				</li>
+			</ul>
+		</div>
+	)
 }
 
 export { MessageList }
