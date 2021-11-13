85c33a47a29210a02f9a73d1c576090f06c5c91f
MSpilari <mb.spilari@hotmail.com>
2021-11-13 13:26:54 -0300

    Fazendo a chamada para Api e escrevendo os seus dados em tela

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,48 +1,44 @@
+import { api } from '../../services/api'
 import logoImg from '../../assets/logo.svg'
 import styles from './styles.module.scss'
+import { useEffect, useState } from 'react'
+
+type Message = {
+	id: string
+	text: string
+	user: {
+		name: string
+		avatar_url: string
+	}
+}
 
 const MessageList = () => {
+	const [messages, setMessages] = useState<Message[]>([])
+
+	useEffect(() => {
+		api
+			.get<Message[]>('messages/last3')
+			.then(response => setMessages(response.data))
+	}, [])
+
 	return (
 		<div className={styles.messageListWrapper}>
 			<img src={logoImg} alt='DoWhile Logo' />
 
 			<ul className={styles.messageList}>
-				<li className={styles.message}>
-					<p className={styles.messageContent}>
-						Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias
-						vel expedita vero culpa eum.
-					</p>
-					<div className={styles.messageUser}>
-						<div className={styles.userImage}>
-							<img src='https://github.com/MSpilari.png' alt='User Image' />
-						</div>
-						<span>MSpilari</span>
-					</div>
-				</li>
-				<li className={styles.message}>
-					<p className={styles.messageContent}>
-						Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias
-						vel expedita vero culpa eum.
-					</p>
-					<div className={styles.messageUser}>
-						<div className={styles.userImage}>
-							<img src='https://github.com/MSpilari.png' alt='User Image' />
-						</div>
-						<span>MSpilari</span>
-					</div>
-				</li>
-				<li className={styles.message}>
-					<p className={styles.messageContent}>
-						Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias
-						vel expedita vero culpa eum.
-					</p>
-					<div className={styles.messageUser}>
-						<div className={styles.userImage}>
-							<img src='https://github.com/MSpilari.png' alt='User Image' />
-						</div>
-						<span>MSpilari</span>
-					</div>
-				</li>
+				{messages.map(message => {
+					return (
+						<li key={message.id} className={styles.message}>
+							<p className={styles.messageContent}>{message.text}</p>
+							<div className={styles.messageUser}>
+								<div className={styles.userImage}>
+									<img src={message.user.avatar_url} alt={message.user.name} />
+								</div>
+								<span>{message.user.name}</span>
+							</div>
+						</li>
+					)
+				})}
 			</ul>
 		</div>
 	)
