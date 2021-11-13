03a9eda2daaa00aa044657aa8dfceba1b8816092
MSpilari <mb.spilari@hotmail.com>
2021-11-13 12:05:06 -0300

    Finalizando o LoginBox

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,3 +1,4 @@
+import { VscGithubInverted } from 'react-icons/vsc'
 import styles from './styles.module.scss'
 
 const LoginBox = () => {
@@ -5,6 +6,7 @@ const LoginBox = () => {
 		<div className={styles.loginBoxWrapper}>
 			<strong>Entre e compartilhe sua mensagem</strong>
 			<a href='#' className={styles.signInWithGithub}>
+				<VscGithubInverted size='24' />
 				Entrar com Github
 			</a>
 		</div>
