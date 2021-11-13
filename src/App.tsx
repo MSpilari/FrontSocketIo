4011ce0c91b5ce292befc4b28d0e22c978b1c7bc
MSpilari <mb.spilari@hotmail.com>
2021-11-13 11:42:10 -0300

    Primeiro commit - Código Base

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import styles from './App.module.scss'
+import { LoginBox } from './components/LoginBox'
+import { MessageList } from './components/MessageList'
+
+const App = () => {
+	return (
+		<main className={styles.contentWrapper}>
+			<MessageList />
+			<LoginBox />
+		</main>
+	)
+}
+
+export { App }
